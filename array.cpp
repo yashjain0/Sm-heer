fc300fc184be4c2101a40de24e1091929315b4e3
Yash Jain <62240937+luciferon@users.noreply.github.com>
2021-03-04 19:13:28 +0530

    add and append added

--- array.cpp
+++ array.cpp
@@ -25,6 +25,22 @@ public:
 			cout<<A[i]<<" ";
 		}
 	}
+	void Append(struct Array *arr,int x)
+ {
+ if(arr->length<arr->size)
+ arr->A[arr->length++]=x;
+ }
+ void Insert(struct Array *arr,int index,int x)
+ {
+ int i;
+
+ if(index>=0 && index <=arr->length)
+ {
+ for(i=arr->length;i>index;i--)
+ arr->A[i]=arr->A[i-1];
+ arr->A[index]=x;
+ arr->length++;
+ }
 
 }
 
