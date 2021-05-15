d52900941cc8173dda2e8498187e9017803bd38a
Yash Jain <62240937+yashjain0@users.noreply.github.com>
2021-05-15 11:42:21 +0530

    Create LL.cpp

---
+++ Linked List/LL.cpp
@@ -0,0 +1,54 @@
+#include <stdio.h>
+#include<stdlib.h>
+
+struct Node
+{
+	int data;
+	struct Node *next;
+}*first=NULL;
+
+void create(int A[], int n)
+{
+	int i;
+	struct Node *t, *last;
+	first=(struct Node *)malloc(sizeof(struct Node));
+	first->data=A[0];
+	first->next=NULL;
+	last=first;
+
+	for(i=1;i<n;i++)
+	{
+		t=(struct Node*)malloc(sizeof(struct Node));
+		t->data=A[i];
+		t->next=NULL;
+		last->next=t;
+		last=t;
+	}
+}
+
+void display(struct Node *p)
+{
+	while(p!=NULL)
+	{
+		printf("%d",p->data);
+		p=p->next;
+	}
+}
+
+void rdisplay(struct Node *p )
+{
+	if(p(!=NULL))
+	{
+		printf("%d",p->data);
+		rdisplay(p->next);
+	}
+}
+
+
+int main()
+{
+	A[]={3,5,7,2,1};
+	create(A,5);
+	display(first);
+	return 0;
+}
