3748562f10de199944af35ed76e8eaa95a88f952
Yash Jain <62240937+yashjain0@users.noreply.github.com>
2021-05-19 15:40:04 +0530

    Update LL.cpp

--- Linked List/LL.cpp
+++ Linked List/LL.cpp
@@ -1,48 +1,46 @@
 #include <stdio.h>
-#include<stdlib.h>
-
+#include <stdlib.h>
 struct Node
 {
-	int data;
-	struct Node *next;
+ int data;
+ struct Node *next;
 }*first=NULL;
-
-void create(int A[], int n)
+void create(int A[],int n)
 {
-	int i;
-	struct Node *t, *last;
-	first=(struct Node *)malloc(sizeof(struct Node));
-	first->data=A[0];
-	first->next=NULL;
-	last=first;
+ int i;
+ struct Node *t,*last;
+ first=(struct Node *)malloc(sizeof(struct Node));
+ first->data=A[0];
+ first->next=NULL;
+ last=first;
 
-	for(i=1;i<n;i++)
-	{
-		t=(struct Node*)malloc(sizeof(struct Node));
-		t->data=A[i];
-		t->next=NULL;
-		last->next=t;
-		last=t;
-	}
+ for(i=1;i<n;i++)
+ {
+ t=(struct Node*)malloc(sizeof(struct Node));
+ t->data=A[i];
+ t->next=NULL;
+ last->next=t;
+ last=t;
+ }
+}
+void Display(struct Node *p)
+{
+ while(p!=NULL)
+ {
+ printf("%d ",p->data);
+ p=p->next;
+ }
+}
+void RDisplay(struct Node *p)
+{
+ if(p!=NULL)
+ {
+ RDisplay(p->next);
+ printf("%d ",p->data);
+
+ }
 }
 
-void display(struct Node *p)
-{
-	while(p!=NULL)
-	{
-		printf("%d",p->data);
-		p=p->next;
-	}
-}
-
-void rdisplay(struct Node *p )
-{
-	if(p(!=NULL))
-	{
-		printf("%d",p->data);
-		rdisplay(p->next);
-	}
-}
 
 int count(struct Node *p)
 {
