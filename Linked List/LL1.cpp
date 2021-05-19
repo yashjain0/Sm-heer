4a9102e69cdccbaad0cfbe7d74a7451f6eb262d1
Yash Jain <62240937+yashjain0@users.noreply.github.com>
2021-05-19 17:03:52 +0530

    Update LL1.cpp

--- Linked List/LL1.cpp
+++ Linked List/LL1.cpp
@@ -43,6 +43,63 @@ void Insert(struct Node *p,int index,int x)
  p->next=t;
 
  }
+}
 
 
+void SortedInsert(struct Node *p,int x)
+{
+ struct Node *t,*q=NULL;
+ 
+ t=(struct Node*)malloc(sizeof(struct Node));
+ t->data=x;
+ t->next=NULL;
+
+ if(first==NULL)
+ first=t;
+ else
+ {
+ while(p && p->data<x)
+ {
+ q=p;
+ p=p->next;
+ }
+ if(p==first)
+ {
+ t->next=first;
+ first=t;
+ }
+ else
+ {
+ t->next=q->next;
+ q->next=t;
+ }
+ }
+
+}
+
+
+
+void Insert(struct Node *p,int index,int x)
+{
+ struct Node *t;
+ int i;
+
+ if(index < 0 || index > count(p))
+ return;
+ t=(struct Node *)malloc(sizeof(struct Node));
+ t->data=x;
+
+ if(index == 0)
+ {
+ t->next=first;
+ first=t;
+ }
+ else
+ {
+ for(i=0;i<index-1;i++)
+ p=p->next;
+ t->next=p->next;
+ p->next=t;
+
+ }
 }
