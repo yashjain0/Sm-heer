932025a05fa04f06bad160657f06d8f7f5d56d3d
Yash Jain <62240937+yashjain0@users.noreply.github.com>
2021-05-19 16:40:19 +0530

    Update LL1.cpp

--- Linked List/LL1.cpp
+++ Linked List/LL1.cpp
@@ -17,3 +17,32 @@ struct Node * LSearch(struct Node *p,int key)
  return NULL;
 
 }
+
+#to insert a node
+void Insert(struct Node *p,int index,int x)
+{
+ struct Node *t;
+ int i;
+ 
+ if(index < 0 || index > count(p))
+ return;
+ t=(struct Node *)malloc(sizeof(struct Node));
+ t->data=x;
+ //to insert at the start
+ if(index == 0)
+ {
+ t->next=first;
+ first=t;
+ }
+ //other than last
+ else
+ {
+ for(i=0;i<index-1;i++)
+ p=p->next;
+ t->next=p->next;
+ p->next=t;
+
+ }
+
+
+}
