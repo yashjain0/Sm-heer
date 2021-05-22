1fd98fed1279f011d8d6d2f45c8c08e54ec008f6
Yash Jain <62240937+yashjain0@users.noreply.github.com>
2021-05-22 12:12:30 +0530

    Update LL2.cpp

--- Linked List/LL2.cpp
+++ Linked List/LL2.cpp
@@ -46,3 +46,60 @@ void Reverse3( struct Node *q, struct Node *q)
 	else
 		first=q;
 }
+
+void create2(int A[],int n)
+{
+ int i;
+ struct Node *t,*last;
+ second=(struct Node *)malloc(sizeof(struct Node));
+ second->data=A[0];
+ second->next=NULL;
+ last=second;
+
+ for(i=1;i<n;i++)
+ {
+ t=(struct Node*)malloc(sizeof(struct Node));
+ t->data=A[i];
+ t->next=NULL;
+ last->next=t;
+ last=t;
+ }
+}
+void Merge(struct Node *p,struct Node *q)
+{
+ struct Node *last;
+ if(p->data < q->data)
+ {
+ third=last=p;
+ p=p->next;
+ third->next=NULL;
+ }
+ else
+ {
+ third=last=q;
+ q=q->next;
+ third->next=NULL;
+ }
+ while(p && q)
+ {
+ if(p->data < q->data)
+ {
+ last->next=p;
+ last=p;
+ p=p->next;
+ last->next=NULL;
+
+ }
+ else
+ {
+ last->next=q;
+ last=q;
+ q=q->next;
+ last->next=NULL;
+
+ }
+ }
+ if(p)last->next=p;
+ if(q)last->next=q;
+
+}
