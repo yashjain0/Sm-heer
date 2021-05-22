de54646da2df1e704df01ffae2f3d1a79559a3cf
Yash Jain <62240937+yashjain0@users.noreply.github.com>
2021-05-22 11:17:26 +0530

    Update LL1.cpp

--- Linked List/LL1.cpp
+++ Linked List/LL1.cpp
@@ -153,3 +153,22 @@ int isSorted(struct Node *p)
 	}
 	return 1;
 }
+
+void RemoveDuplicate( struct Node *p)
+{
+	struct Node *q=p->next;
+	while(q!=NULL)
+	{
+		if(p->data!=q->data)
+		{
+			p=q;
+			q=q->next;
+		}
+		else
+		{
+			p->next=q->next;
+			free(q);
+			q=p->next;
+		}
+	}
+}
