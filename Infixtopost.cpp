81d55b8c041d8322e99e5eb512f2caa45d1e5016
Yash Jain <62240937+yashjain0@users.noreply.github.com>
2021-06-02 11:08:46 +0530

    Update Infixtopost.cpp

--- Infixtopost.cpp
+++ Infixtopost.cpp
@@ -1,128 +1,105 @@
-//Infix to Postfix Conversion
-#include <stdio.h>
-#include <stdlib.h>
-#include<strings.h>
-struct Node
-{
- char data;
- struct Node *next;
-}*top=NULL;
-void push(char x)
-{
- struct Node *t;
- t=(struct Node*)malloc(sizeof(struct Node));
+#include <iostream>
+#include <stack>
+#include <cstring>
 
- if(t==NULL)
- printf("stack is full\n");
- else
- {
- t->data=x;
- t->next=top;
- top=t;
- }
+using namespace std;
 
-}
-char pop()
-{
- struct Node *t;
- char x=-1;
-
- if(top==NULL)
- printf("Stack is Empty\n");
- else
- {
- t=top;
- top=top->next;
- x=t->data;
- free(t);
- }
- return x;
-}
-void Display()
-{
- struct Node *p;
- p=top;
- while(p!=NULL)
- {
- printf("%d ",p->data);
- p=p->next;
- }
- printf("\n");
-}
-int isBalanced(char *exp)
-{
- int i;
-
- for(i=0;exp[i]!='\0';i++)
- {
- if(exp[i]=='(')
- push(exp[i]);
- else if(exp[i]==')')
- {
- if(top==NULL)
- return 0;
- pop();
- }
- }
- if(top==NULL)
- return 1;
- else
- return 0;
-}
-int pre(char x)
-{
- if(x=='+' || x=='-')
- return 1;
- else if(x=='*' || x=='/')
- return 2;
- return 0;
+ont isOperand(char x){
+    if(x=='+'||x=='-'||x=='*'||x=='/'||x=='^'||x=='('||x==')')
+    {return 0;}
+    return 1;
 }
 
-int isOperand(char x)
+int outPre(char x)
 {
- if(x=='+' || x=='-' || x=='*' || x=='/')
- return 0;
- else
- return 1;
-
+    if(x=='+'||x=='-')
+    {
+        return 1;
+    }
+    else if(x=='*'||x=='/')
+    {
+        return 3;
+    }
+    else if(x=='^')
+    {
+        return 6;
+    }
+    else if(x=='(')
+    {
+        return 7;
+    }
+    else if(x==')')
+    {
+        return 0;
+    }
+    return -1;
 }
 
-char * InToPost(char *infix)
+int inPre(char x)
 {
- int i=0,j=0;
- char *postfix;
- int len=strlen(infix);
- postfix=(char *)malloc((len+2)*sizeof(char));
+    if(x=='+'||x=='-')
+    {
+        return 2;
+    }
+    else if(x=='*'||x=='/')
+    {
+        return 4;
+    }
+    else if(x=='^')
+    {
+        return 5;
+    }
+    else if(x=='(')
+    {
+        return 0;
+    }
+   
+    return -1;
+}
 
- while(infix[i]!='\0')
- {
- if(isOperand(infix[i]))
- postfix[j++]=infix[i++];
- else
- {
- if(pre(infix[i])>pre(top->data))
- push(infix[i++]);
- else
- {
- postfix[j++]=pop();
- }
- }
- }
- while(top!=NULL)
- postfix[j++]=pop();
- postfix[j]='\0';
- return postfix;
+char* convert(char* infix)
+{
+    char* postfix = new char [strlen(infix)+1];
+
+    stack<char> stk;
+
+    int i=0;
+    int j=0;
+
+    while(infix[i]!='\0')
+    {
+        if(isOperand(infix[i]))
+        {
+            postfix[j++]=infix[i++];
+        }
+        else
+        {
+            if(stk.empty()||outPre(infix[i])>inPre(stk.top()))
+            {stk.push(infix[i++]);}
+            else if(outPre(infix[i])==inPre(stk.top()))
+            {stk.pop();}
+            else
+            {
+                postfix[j++]=stk.top();
+                stk.pop();
+            }
+        }
+
+        while(!stk.empty() && stk.top() != ')')
+        {
+            postfix[j++]=stk.top();
+            stk.pop();
+        }
+        postfix[j]='\0';
+        stk.pop();
+    }
+    postfix[j]='\0';
+    return postfix;
 }
 
 int main()
 {
- char *infix="a+b*c-d/e";
- push('#');
-
- char *postfix=InToPost(infix);
-
- printf("%s ",postfix);
-
-
- return 0;
+  char infix[] = "((a+b)*c)-d^e^f";
+ 
+    cout << convert(infix) << endl;
 }
