ab09682842442a1a368dffc29a6dad4064305ffb
Yash Jain <62240937+yashjain0@users.noreply.github.com>
2021-06-14 18:11:56 +0530

    Update Iterativetraversal.cpp

--- BTree/Iterativetraversal.cpp
+++ BTree/Iterativetraversal.cpp
@@ -202,6 +202,29 @@ void Tree::iterativePostorder(Node *p) {
     cout << endl;
 }
  
+void LevelOrder(struct Node *root)
+{
+ struct Queue q;
+ create(&q,100);
+
+ printf("%d ",root->data);
+ enqueue(&q,root);
+
+ while(!isEmpty(q))
+ {
+ root=dequeue(&q);
+ if(root->lchild)
+ {
+ printf("%d ",root->lchild->data);
+ enqueue(&q,root->lchild);
+ }
+ if(root->rchild)
+ {
+ printf("%d ",root->rchild->data);
+ enqueue(&q,root->rchild);
+ }
+ }
+}
  
 int main() {
  
