94239646ce8697abab8f54949b9a4e58b6d0cd2c
Yash Jain <62240937+yashjain0@users.noreply.github.com>
2021-07-30 16:42:56 +0530

    Update MaxHeapInsert.cpp

--- Heap/MaxHeapInsert.cpp
+++ Heap/MaxHeapInsert.cpp
@@ -20,3 +20,26 @@ void create()
 		insert(A,i);
 	}
 }
+void delete(int A[], int n)
+{
+	int x,i,j;
+	x=A[n];
+	A[1]=A[n];
+	i=1; j=2*i;
+	while(j<n-1)
+	{
+		if(A[j]<A[j+1])
+		{
+			j=j+1;
+		}
+		if(A[j]>A[i])
+		{
+			swap(A[i],A[j]);
+			i=j;
+			j=2*j;
+		}
+		else
+			break;
+	}
+
+}
